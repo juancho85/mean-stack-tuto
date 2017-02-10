51ac7d1652552184ffef4554504b91f3959da951
Juan David Sánchez <juancho85@gmail.com>
2017-02-10 20:02:39 +0100

    create models with mongoose and provide unique validation though mongoose-unique-validator

---
+++ models/message.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+   content: {type: String, required: true},
+   user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+//the default collection name will be messages (plural form all lowercase)
+module.exports = mongoose.module('Message', schema);
